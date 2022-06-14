e775670b86ee9c463a47a0bd6d9125e39db1157c
larissapessoa <lsp@icomp.ufam.edu.br>
2022-06-13 20:33:16 -0400

    adding reading on motion sensor

--- app/src/main/java/com/example/titansensorlocal/MainActivity.java
+++ app/src/main/java/com/example/titansensorlocal/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.titansensorlocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     private ToggleButton toggleButton;
     private Button updateLocation;
+    private SensorManager sensorManager;
     TextView latitudeTextView, longitTextView;
 
     @Override
@@ -19,8 +27,26 @@ public class MainActivity extends AppCompatActivity {
         latitudeTextView = findViewById(R.id.latTextView);
         longitTextView = findViewById(R.id.lonTextView);
         updateLocation = findViewById(R.id.updateLocation);
-        toggleButton = findViewById(R.id.onOffFlashlight);
 
-
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float xValue = Math.abs(event.values[0]);
+            float yValue = Math.abs(event.values[1]);
+            float zValue = Math.abs(event.values[2]);
+            if (xValue > 15 || yValue > 15 || zValue > 15) {
+                Toast.makeText(MainActivity.this, "shake function activated", Toast.LENGTH_SHORT).show();
+                Log.d("SENSOR", "Balancou...");
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
 }
